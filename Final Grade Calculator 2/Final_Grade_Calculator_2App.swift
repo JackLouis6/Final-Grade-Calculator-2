af1494dfa02126105a6651f2c3cb7bf39ff7f57b
Jack Louis <jbl5925@gmail.com>
2021-10-12 10:55:12 -0500

    Initial Commit

---
+++ Final Grade Calculator 2/Final_Grade_Calculator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_Calculator_2App.swift
+//  Final Grade Calculator 2
+//
+//  Created by Student on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_Calculator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
